16bc53fd69fc54733e904bae363011f74743d2ef
SantiagoYB <98602870+SantiagoYB@users.noreply.github.com>
2023-07-30 18:42:32 -0500

    Update risk.cpp
    Comentarios

--- risk.cpp
+++ risk.cpp
@@ -1,13 +1,33 @@
-//Compilar ->  g++ -std=c++11 risk.cxx risk.cpp -o ejecutable
-//ejecutar -> ./ejecutable
+//*****************************************************************
+// Autores: Juan Diego Echeverry, Santiago Yañez y Nicolás Rincón
+// Proyecto juego risk - Estructuras de datos
+// Compilación: "g++ -std=c++11 risk.cxx risk.cpp -o ejecutable"
+// Ejecución: "./ejecutable"
+// Observaciones: Los piolines gobernaran el mundo
+// 
+//
+//  Archivo creado por Juan Diego Echeverry, Santiago Yañez y Nicolás Rincón
+//  Fecha de inicio: 28/07/23
+//  Fecha de finalización: ---
+//*****************************************************************
 
+
+//*****************************************************************
+// INICIO - LIBRERIAS INCLUIDAS
+//*****************************************************************
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <vector>
-
 #include "risk.h"
+//*****************************************************************
+// FIN - LIBRERIAS INCLUIDAS
+//*****************************************************************
 
+
+//*********************************************************************************************************
+//  INICIO - PROGRAMA PRINCIPAL
+//*********************************************************************************************************
 int main()
 {
   std::string input, palabra, comando;
@@ -75,5 +95,8 @@ int main()
 
   return 0;
 }
+//*********************************************************************************************************
+//  FIN - PROGRAMA PRINCIPAL
+//*********************************************************************************************************
 
 // eof - risk.cpp
