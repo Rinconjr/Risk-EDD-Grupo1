9613eefede14ddda9982616272fdc4035093a813
SantiagoYB <98602870+SantiagoYB@users.noreply.github.com>
2023-07-30 18:46:28 -0500

    Update risk.h
    Se agregaron comentarios

--- risk.h
+++ risk.h
@@ -1,9 +1,33 @@
+//*****************************************************************
+// Autores: Juan Diego Echeverry, Santiago Yañez y Nicolás Rincón
+// Proyecto juego risk - Estructuras de datos
+// Compilación: "g++ -std=c++11 risk.cpp -o ejecutable"
+// Ejecución: "./ejecutable"
+// 
+//
+//  Archivo creado por Juan Diego Echeverry, Santiago Yañez y Nicolás Rincón
+//  Fecha de inicio: 28/07/23
+//  Fecha de finalización: ---
+//*****************************************************************
+
+
 #ifndef RISK_H
 #define RISK_H
 
+
+//*****************************************************************
+// INICIO - LIBRERIAS INCLUIDAS
+//*****************************************************************
 #include <string>
 #include <vector>
+//*****************************************************************
+// FIN - LIBRERIAS INCLUIDAS
+//*****************************************************************
 
+
+//*****************************************************************
+// INICIO - FIRMA FUNCIONES
+//*****************************************************************
 void comando_inicializar();
 void comando_turno();
 void comando_guardar();
@@ -12,6 +36,10 @@ void comando_costo_conquista();
 void comando_conquista_mas_barata();
 void comando_ayuda();
 void comando_ayuda_comandos(const std::string& comando);
+//*****************************************************************
+// FIN - FIRMA FUNCIONES
+//*****************************************************************
+
 
 #endif // RISK_H
 
