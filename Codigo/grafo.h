b75b83d50735ff6aebe05a8305e9d993c8abca59
Rinconjr <nicorinconb@yahoo.com>
2023-11-17 19:22:08 -0500

    Agregando funcionalidades basicas al grafo

--- Codigo/grafo.h
+++ Codigo/grafo.h
@@ -5,10 +5,20 @@
 
 class Grafo{
     private:
+        std::vector<std::string> paises;
         std::vector<std::vector<int>> matriz_adyacencia;
     public:
         Grafo();
+        void setMatriz_Adyacencia(std::vector<std::vector<int>> m);
         std::vector<std::vector<int>> obtenerMatriz();
+        void mostrarMatrizAdyacencia();
+        void setPaises(std::vector<std::string> p);
+        std::vector<std::string> obtenerPaises();
+        void mostrarPaises();
+        int obtenerPosicionPais(std::string pais);
+        bool insertarArista(std::string origen, std::string destino, int costo);
+        bool eliminarArista(std::string origen, std::string destino);
+
 };
 
 #include "grafo.hxx"
--- Codigo/grafo.hxx
+++ Codigo/grafo.hxx
@@ -1,6 +1,7 @@
 #include "grafo.h"
 
 #include <vector>
+#include <iostream>
 
 Grafo::Grafo() {
     this->matriz_adyacencia = { {0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
@@ -46,8 +47,74 @@ Grafo::Grafo() {
                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1},
                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0}
                             };
+
+    this->paises = {"Alaska","Alberta","America Central","Estados Unidos Orientales","Groenlandia","Territorio Noroccidental","Ontario","Quebec","Estados Unidos Occidentales","Argentina","Brasil","Peru","Venezuela","Gran Bretana","Islandia","Europa del Norte","Escandinavia","Europa del Sur","Ucrania","Europa Occidental","Congo","Africa Oriental","Egipto","Madagascar","Africa del Norte","Africa del Sur","Afghanistan","China","India","Irkutsk","Japon","Kamchatka","Medio Oriente","Mongolia","Siam","Siberia","Ural","Yakutsk","Australia Oriental","Indonesia","Nueva Guinea","Australia Occidental"};
+}
+
+void Grafo::setPaises(std::vector<std::string> p) {
+    this->paises = p;
+}
+
+std::vector<std::string> Grafo::obtenerPaises() {
+    return this->paises;
+}
+
+void Grafo::mostrarPaises(){
+    for (int i = 0; i < this->paises.size(); i++){
+        std::cout << this->paises[i] << std::endl;
+    }
+}
+
+void Grafo::setMatriz_Adyacencia(std::vector<std::vector<int>> m) {
+    this->matriz_adyacencia = m;
 }
 
 std::vector<std::vector<int>> Grafo::obtenerMatriz() {
     return this->matriz_adyacencia;
+}
+
+void Grafo::mostrarMatrizAdyacencia(){
+    for (int i = 0; i < this->matriz_adyacencia.size(); i++){
+        for (int j = 0; j < this->matriz_adyacencia[i].size(); j++){
+            std::cout << this->matriz_adyacencia[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int Grafo::obtenerPosicionPais(std::string pais) {
+    for (int i = 0; i < this->paises.size(); i++){
+        if (this->paises[i] == pais){
+            return i; //Si encuentra el pais, retorna su posicion en el vector de paises.
+        }
+    }
+    return -1; //Caso contrario, retorna -1.
+}
+
+bool Grafo::insertarArista(std::string origen, std::string destino, int costo) {
+    int posOrigen = this->obtenerPosicionPais(origen);
+    int posDestino = this->obtenerPosicionPais(destino);
+
+    if (posOrigen == -1 || posDestino == -1){ //Si no se encontro o el origen o el destino, retorna falso
+        return false;
+    }
+
+    //Caso contrario, actualiza la matriz de adyacencia con el costo de la arista y retorna verdadero.
+    this->matriz_adyacencia[posOrigen][posDestino] = costo;
+    this->matriz_adyacencia[posDestino][posOrigen] = costo;
+    return true;
+}
+
+bool Grafo::eliminarArista(std::string origen, std::string destino) {
+    int posOrigen = this->obtenerPosicionPais(origen);
+    int posDestino = this->obtenerPosicionPais(destino);
+
+    if (posOrigen == -1 || posDestino == -1){ //Si no se encontro o el origen o el destino, retorna falso
+        return false;
+    }
+
+    //Caso contrario, actualiza la matriz de adyacencia con el costo de la arista y retorna verdadero.
+    this->matriz_adyacencia[posOrigen][posDestino] = 0;
+    this->matriz_adyacencia[posDestino][posOrigen] = 0;
+    return true;
 }
